916592e8d6d9bcbe3528474e95eae1a01aefc3b7
Miti <miti.shah@nium.com>
2020-11-18 12:20:44 +0530

    Button themes added.

--- src/UIButton/index.js
+++ src/UIButton/index.js
@@ -1,15 +1,22 @@
-import React, { useState } from 'react'
-import { View, Text, TouchableOpacity, ViewPropTypes, Image } from 'react-native'
-import { BUTTON_TYPES, BUTTON_SHAPES } from '../utils/Constants'
-import { APPLIED_THEME as Theme } from '../utils/Constants'
-import renderIf from '../utils/RenderIf'
-import { ButtonStyles } from './ButtonStyles'
-import PropTypes from 'prop-types'
-import LinearGradient from 'react-native-linear-gradient'
-import { replaceSpaceWithUnderscore, accessibilityId } from '../utils/index'
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ViewPropTypes,
+  Image,
+} from "react-native";
+import { BUTTON_TYPES, BUTTON_SHAPES } from "../utils/Constants";
+import { APPLIED_THEME as Theme } from "../utils/Constants";
+import renderIf from "../utils/RenderIf";
+import { ButtonStyles } from "./ButtonStyles";
+import PropTypes from "prop-types";
+import LinearGradient from "react-native-linear-gradient";
+import { replaceSpaceWithUnderscore, accessibilityId } from "../utils/index";
 
 function UIButton(props) {
-  var { content,
+  var {
+    content,
     buttonType,
     style,
     buttonShape,
@@ -22,15 +29,17 @@ function UIButton(props) {
     iconStyle,
     contentStyle,
     onPressOut,
-  linkStyle } = props
-  const [buttonPressedIn, setButtonPressedIn] = useState(false)
-  var buttonStyle
-  var buttonShape = buttonShape || 'default'
-  var buttonType = buttonType || 'default'
-  var isDisabled = disabled || false
-  var isIconLeftPositioned = iconLeftPositioned || false
-  var buttonBackgroundColor
-  var buttonBorderColor
+    linkStyle,
+  } = props;
+
+  const [buttonPressedIn, setButtonPressedIn] = useState(false);
+  var buttonStyle;
+  var buttonShape = buttonShape || "default";
+  var buttonType = buttonType || "default";
+  var isDisabled = disabled || false;
+  var isIconLeftPositioned = iconLeftPositioned || false;
+  var buttonBackgroundColor;
+  var buttonBorderColor;
 
   const isGradient = buttonType === BUTTON_TYPES.gradient;
 
@@ -70,13 +79,17 @@ function UIButton(props) {
       break;
   }
 
-  var textColor = getTextColor(buttonPressedIn, buttonType, isDisabled);
-  var iconColor = getImageColor(buttonType, isDisabled)
+  var textColor = getTextColor(buttonPressedIn, buttonType, isDisabled, props);
+  var iconColor = getImageColor(buttonType, isDisabled, props);
   let buttonAttributes = { buttonStyle, buttonShape, textColor };
 
   if (!isGradient) {
-    buttonBackgroundColor = getButtonBackgroundColor(buttonType, isDisabled);
-    buttonBorderColor = getButtonBorderColor(buttonType, isDisabled);
+    buttonBackgroundColor = getButtonBackgroundColor(
+      buttonType,
+      isDisabled,
+      props
+    );
+    buttonBorderColor = getButtonBorderColor(buttonType, isDisabled, props);
     buttonAttributes = {
       ...buttonAttributes,
       buttonBackgroundColor,
@@ -89,18 +102,20 @@ function UIButton(props) {
       buttonStyle: ButtonStyles.linkButtonStyle,
       buttonShape: null,
       textColor: ButtonStyles.linkButtonTextStyle.color,
-      buttonBackgroundColor: 'transparent',
-      buttonBorderColor: 'transparent',
+      buttonBackgroundColor: "transparent",
+      buttonBorderColor: "transparent",
       linkTextStyle: ButtonStyles.linkButtonTextStyle,
-      linkStyle: linkStyle
-    }
+      linkStyle: linkStyle,
+    };
   }
 
   if (props.buttonType === BUTTON_TYPES.primary) {
     buttonAttributes = {
       ...buttonAttributes,
-      titleTextStyle: props.isTitleCapital ? { textTransform: 'uppercase' } : { textTransform: 'capitalize' }
-    }
+      titleTextStyle: props.isTitleCapital
+        ? { textTransform: "uppercase" }
+        : { textTransform: "capitalize" },
+    };
   }
 
   return isGradient
@@ -121,53 +136,58 @@ function UIButton(props) {
           onPressIn={() => btnPressedIn()}
           onPressOut={() => btnPressedOut()}
           accessibilityLabel={accessibilityId(
-            getButtonAccessibilityLabel(content || 'icon'), content
+            getButtonAccessibilityLabel(content || "icon"),
+            content
           )}
-          testID={getButtonAccessibilityLabel(content || 'icon')}
+          testID={getButtonAccessibilityLabel(content || "icon")}
         >
           <LinearGradient
             start={{ x: 0, y: 0 }}
             end={{ x: 1, y: 0 }}
             colors={[
-              Theme.GradientButton.backgroundColorGradientStart,
-              Theme.GradientButton.backgroundColorGradientMid,
-              Theme.GradientButton.backgroundColorGradientEnd,
+              props.theme.GradientButton.backgroundColorGradientStart,
+              props.theme.GradientButton.backgroundColorGradientMid,
+              props.theme.GradientButton.backgroundColorGradientEnd,
             ]}
-            style={[
-              buttonStyle,
-              buttonShape,
-              gradientStyle,
-            ]}
+            style={[buttonStyle, buttonShape, gradientStyle]}
           >
-            {
-              renderIf(content || iconWithBtnText)(
-                <View style={[ButtonStyles.contentWithIcon, contentStyle && contentStyle,
-                { flexDirection: isIconLeftPositioned ? 'row-reverse' : 'row' }]}>
-                  {renderIf(content)(
+            {renderIf(content || iconWithBtnText)(
+              <View
+                style={[
+                  ButtonStyles.contentWithIcon,
+                  contentStyle && contentStyle,
+                  {
+                    flexDirection: isIconLeftPositioned ? "row-reverse" : "row",
+                  },
+                ]}
+              >
+                {renderIf(content)(
                   <View style={[ButtonStyles.titleView]}>
                     <Text
-                      style={[ButtonStyles.title,
-                      { color: textColor },
-                        titleStyle
+                      style={[
+                        ButtonStyles.title,
+                        { color: textColor },
+                        titleStyle,
                       ]}
                       numberOfLines={1}
-                    > {content}
+                    >
+                      {" "}
+                      {content}
                     </Text>
                   </View>
-                  )}
-                  {renderIf(iconWithBtnText)(
-                    <View style={{ justifyContent: 'center' }}>
-                      {iconWithBtnText}
-                    </View>
-                  )}
-                </View>
-              )
-            }
+                )}
+                {renderIf(iconWithBtnText)(
+                  <View style={{ justifyContent: "center" }}>
+                    {iconWithBtnText}
+                  </View>
+                )}
+              </View>
+            )}
             {props.children}
           </LinearGradient>
         </TouchableOpacity>
       </>
-    )
+    );
   }
 
   function getNormalButton(buttonAttributes) {
@@ -179,7 +199,7 @@ function UIButton(props) {
       buttonBorderColor,
       linkTextStyle,
       titleTextStyle,
-      linkStyle
+      linkStyle,
     } = buttonAttributes;
     return (
       <>
@@ -189,7 +209,7 @@ function UIButton(props) {
             buttonStyle,
             buttonShape,
             passStyles(buttonBackgroundColor, buttonBorderColor),
-            style
+            style,
           ]}
           disabled={disabled ? disabled : false}
           activeOpacity={1.0}
@@ -198,34 +218,45 @@ function UIButton(props) {
           onPressIn={() => btnPressedIn()}
           onPressOut={() => btnPressedOut()}
           accessibilityLabel={accessibilityId(
-            getButtonAccessibilityLabel(content || 'icon'), content
+            getButtonAccessibilityLabel(content || "icon"),
+            content
           )}
-          testID={getButtonAccessibilityLabel(content || 'icon')}
+          testID={getButtonAccessibilityLabel(content || "icon")}
         >
           {renderIf(content || iconWithBtnText)(
-            <View style={[ButtonStyles.buttonContainer, 
-              iconWithBtnText && iconWithBtnText ? ButtonStyles.contentWithIcon : null, 
-              contentStyle && contentStyle,
-            { flexDirection: isIconLeftPositioned ? 'row-reverse' : 'row' }]}>
+            <View
+              style={[
+                ButtonStyles.buttonContainer,
+                iconWithBtnText && iconWithBtnText
+                  ? ButtonStyles.contentWithIcon
+                  : null,
+                contentStyle && contentStyle,
+                { flexDirection: isIconLeftPositioned ? "row-reverse" : "row" },
+              ]}
+            >
               {renderIf(content)(
-              <View style={[props.buttonType === 'link' ? null : ButtonStyles.titleView]}>
-                <Text
+                <View
                   style={[
-                    ButtonStyles.title,
-                    { color: textColor },
-                    linkTextStyle,
-                    titleTextStyle,
-                    titleStyle,
-                    linkStyle
+                    props.buttonType === "link" ? null : ButtonStyles.titleView,
                   ]}
-                  numberOfLines={1}
                 >
-                  {content}
-                </Text>
-              </View>
+                  <Text
+                    style={[
+                      ButtonStyles.title,
+                      { color: textColor },
+                      linkTextStyle,
+                      titleTextStyle,
+                      titleStyle,
+                      linkStyle,
+                    ]}
+                    numberOfLines={1}
+                  >
+                    {content}
+                  </Text>
+                </View>
               )}
               {renderIf(iconWithBtnText)(
-                <View style={{ justifyContent: 'center' }}>
+                <View style={{ justifyContent: "center" }}>
                   {iconWithBtnText}
                 </View>
               )}
@@ -242,21 +273,21 @@ function UIButton(props) {
       backgroundColor: buttonBackgroundColor,
       borderColor: buttonBorderColor,
       opacity: buttonType === BUTTON_TYPES.disabled ? 0.5 : 1,
-    }
+    };
   }
 
   function btnPressedIn() {
     if (!disabled) {
-      setButtonPressedIn(true)
+      setButtonPressedIn(true);
     }
     if (onPressIn !== undefined) {
-      onPressIn()
+      onPressIn();
     }
   }
 
   function btnPressedOut() {
     if (!disabled) {
-      setButtonPressedIn(false)
+      setButtonPressedIn(false);
     }
     if (onPressOut !== undefined) {
       onPressOut();
@@ -264,50 +295,52 @@ function UIButton(props) {
   }
 }
 
-function getButtonBackgroundColor(buttonType, isDisabled) {
-  const buttonTheme = getThemeForButtonType(buttonType, isDisabled)
-  return buttonTheme.backgroundColor
+function getButtonBackgroundColor(buttonType, isDisabled, props) {
+  const buttonTheme = getThemeForButtonType(buttonType, isDisabled, props);
+  return buttonTheme.backgroundColor;
 }
 
-function getTextColor(buttonPressedIn, buttonType, isDisabled) {
-  const buttonTheme = getThemeForButtonType(buttonType, isDisabled)
+function getTextColor(buttonPressedIn, buttonType, isDisabled, props) {
+  const buttonTheme = getThemeForButtonType(buttonType, isDisabled, props);
   let textColor = buttonTheme.textColor;
   if (buttonPressedIn) {
     textColor = buttonTheme.activeTextColor;
   }
-  return textColor
+  return textColor;
 }
 
-function getImageColor(buttonType, isDisabled) {
-  const buttonTheme = getThemeForButtonType(buttonType, isDisabled)
-  let iconColor = buttonTheme.tintColor
-  return iconColor
+function getImageColor(buttonType, isDisabled, props) {
+  const buttonTheme = getThemeForButtonType(buttonType, isDisabled, props);
+  let iconColor = buttonTheme.tintColor;
+  return iconColor;
 }
 
-function getThemeForButtonType(buttonType, isDisabled) {
+function getThemeForButtonType(buttonType, isDisabled, props) {
   if (buttonType === BUTTON_TYPES.primary) {
-    return isDisabled ? Theme.DisabledButton : Theme.PrimaryButton
+    return isDisabled ? props.theme.DisabledButton : props.theme.PrimaryButton;
   } else if (buttonType === BUTTON_TYPES.secondary) {
-    return isDisabled ? Theme.DisabledSecondaryButton : Theme.SecondaryButton;
+    return isDisabled
+      ? props.theme.DisabledSecondaryButton
+      : props.theme.SecondaryButton;
   } else if (buttonType === BUTTON_TYPES.disabled) {
-    return Theme.DisabledButton;
+    return props.theme.DisabledButton;
   } else if (buttonType === BUTTON_TYPES.gradient) {
-    return Theme.GradientButton;
+    return props.theme.GradientButton;
   } else if (buttonType === BUTTON_TYPES.link) {
-    return Theme.LinkButton
+    return props.theme.LinkButton;
   }
-  return Theme.DefaultButton;
+  return props.theme.DefaultButton;
 }
 
-function getButtonBorderColor(buttonType, isDisabled) {
-  const buttonTheme = getThemeForButtonType(buttonType, isDisabled);
+function getButtonBorderColor(buttonType, isDisabled, props) {
+  const buttonTheme = getThemeForButtonType(buttonType, isDisabled, props);
   return buttonTheme.borderColor;
 }
 
 function getButtonAccessibilityLabel(content) {
-  let label = ''
-  label = `${replaceSpaceWithUnderscore(content)}_button`
-  return label
+  let label = "";
+  label = `${replaceSpaceWithUnderscore(content)}_button`;
+  return label;
 }
 UIButton.propTypes = {
   theme: PropTypes.object,
@@ -315,27 +348,27 @@ UIButton.propTypes = {
   buttonType: PropTypes.oneOf(Object.keys(BUTTON_TYPES)),
   buttonShape: PropTypes.oneOf(Object.keys(BUTTON_SHAPES)),
   titleStyle: PropTypes.shape({
-    ...ViewPropTypes.style
+    ...ViewPropTypes.style,
   }),
   gradientStyle: PropTypes.shape({
-    ...ViewPropTypes.style
+    ...ViewPropTypes.style,
   }),
   linkStyle: PropTypes.shape({
-    ...ViewPropTypes.style
+    ...ViewPropTypes.style,
   }),
   contentStyle: PropTypes.shape({
-    ...ViewPropTypes.style
+    ...ViewPropTypes.style,
   }),
   iconStyle: PropTypes.shape({
-    ...ViewPropTypes.style
+    ...ViewPropTypes.style,
   }),
   iconLeftPositioned: PropTypes.bool,
   onPressIn: PropTypes.func,
-  onPressOut: PropTypes.func
+  onPressOut: PropTypes.func,
 };
 
 UIButton.defaultProps = {
-  theme: Theme
+  theme: Theme,
 };
 
-export default UIButton
+export default UIButton;
